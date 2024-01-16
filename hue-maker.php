4a61cb106a334dc98dcd868607dd7218dfc03161
PocoPota <pocopota.program@gmail.com>
2024-01-16 15:32:59 +0900

    Add files via upload

--- hue-maker.php
+++ hue-maker.php
@@ -1,4 +1,11 @@
 <?php
+// キャッシュ無効化
+header( 'Expires: Thu, 01 Jan 1970 00:00:00 GMT' );
+header( 'Last-Modified: '.gmdate( 'D, d M Y H:i:s' ).' GMT' );
+header( 'Cache-Control: no-store, no-cache, must-revalidate,max-age=0' );
+header( 'Cache-Control: post-check=0, pre-check=0', false );
+header( 'Pragma: no-cache' );
+
 // パラメータ取得
 $width = isset($_GET['w']) ? intval($_GET['w']) : 400;
 $height = isset($_GET['h']) ? intval($_GET['h']) : 300;
