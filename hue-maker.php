0964684b1355f71c4b1a16dfa0ae14b0b9c73fd3
PocoPota <pocopota.program@gmail.com>
2024-01-19 20:15:11 +0900

    クロスオリジン許可

--- hue-maker.php
+++ hue-maker.php
@@ -1,4 +1,7 @@
 <?php
+// クロスサイト許可
+header("Access-Control-Allow-Origin: *");
+
 // キャッシュ無効化
 header( 'Expires: Thu, 01 Jan 1970 00:00:00 GMT' );
 header( 'Last-Modified: '.gmdate( 'D, d M Y H:i:s' ).' GMT' );
